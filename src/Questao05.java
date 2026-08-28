da3a5d01631b9b3f696ad3c300d929689859a971
diegoarthurj <diegoarthurj13@gmail.com>
2026-08-28 19:35:00 -0300

    Adicionando questoes 4, 5 e 6

---
+++ src/Questao05.java
@@ -0,0 +1,16 @@
+public class Questao05 {
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            for (int i = 1; i <= 3; i++) {
+                System.out.println(i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+
+        t.start();
+    }
+}
---
+++ src/Questao06.java
@@ -0,0 +1,13 @@
+public class Questao06 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(i);
+            }
+        });
+
+        t.start();
+        t.join();
+        System.out.println("Fim");
+    }
+}
